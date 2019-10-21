3575bed677e500daceec46170de12fc68b5cc9fe
Gonzalo Lopez Garcia <lopezg16@gonzalos-mbp.wireless.mountsinai.org>
2019-10-21 14:34:52 -0400

    first commit

---
+++ R/chr.arm.cnv.r
@@ -0,0 +1,77 @@
+#' 
+#'
+#' Obtain a matrix with the weighted average CN per chromosome arm 
+#' @param seg (data.frame) segmentation data with 6 columns: sample, chromosome, start, end, probes, segment_mean
+#' @param hg (hg19 or hg38) reference genome version to draw chromosome limits and centromeres
+#' @keywords CNV, segmentation, chromosome arm
+#' @export
+#' @examples
+#' chr.arm.cnv()
+
+
+chr.arm.cnv <- function(seg,
+                    genome.v="hg19",
+                    verbose=TRUE){
+
+  require(D3GB,quietly = TRUE,warn.conflicts = FALSE)
+  require(taRifx,quietly = TRUE,warn.conflicts = FALSE)
+  require(tidyr,quietly = TRUE,warn.conflicts = FALSE)
+  require(GenomicRanges,quietly = TRUE,warn.conflicts = FALSE)
+  
+  segdat <- validate.seg(seg)
+  
+  if(genome.v %in% c("GRCh37","hg19")){ 
+    bands <- GRCh37.bands
+  }else if(genome.v %in% c("GRCh38","hg38")){ 
+    bands <- GRCh38.bands
+  }else{stop("Genome version not provided")}
+  
+  centromeres_start <- bands[intersect(which(bands$score == "acen"),grep("q",bands$name)),"start"]
+  centromeres_end <- bands[intersect(which(bands$score == "acen"),grep("q",bands$name)),"end"]
+  names(centromeres_start) <-  names(centromeres_end) <- paste("chr",bands[intersect(which(bands$score == "acen"),grep("q",bands$name)),"chr"],sep="")
+  
+  chrends <- chromosome.limit.coords(segdat)
+  chrarms <- rbind(cbind(chrends$begin,centromeres_start[rownames(chrends)]),cbind(centromeres_end[rownames(chrends)],chrends$end))
+  chrarms <- data.frame(rownames(chrarms),chrarms,c(paste(rownames(chrends),"p",sep=""), paste(rownames(chrends),"q",sep="")))
+  colnames(chrarms) <- c("chr","start","end","arm")
+  
+  chrarms <- remove.factors(chrarms[which(chrarms$end -chrarms$start > 0),])
+  rownames(chrarms) <- NULL
+  
+  chrarmsGR <- with(chrarms,GRanges(chr, IRanges(start=start, end=end)))
+
+  segdat_gr <- with(segdat, GRanges(chrom, IRanges(start=start, end=end)))
+  hits <-GenomicAlignments::findOverlaps(chrarmsGR,segdat_gr)
+  
+  armcnvmat <- matrix(ncol=length(unique(segdat$sample)), nrow=nrow(chrarms) )
+  colnames(armcnvmat) <- unique(segdat$sample)
+  rownames(armcnvmat) <- chrarms$arm
+  
+  for(i in unique(queryHits(hits))){ 
+    arm <- chrarms[i,"arm"]
+    
+    if(verbose) message(arm)
+    
+    armdf <- segdat[subjectHits(hits)[which(queryHits(hits) == i)],]
+    armdf[which(armdf[,"start"] < chrarms[i,"start"]),"start"] <- chrarms[i,"start"]
+    armdf[which(armdf[,"end"] > chrarms[i,"end"]),"end"] <- chrarms[i,"end"]
+
+    arm.width <- armdf[,"end"] - armdf[,"start"]
+    armdf <- data.frame(armdf,arm.width)
+    armlength <- aggregate(arm.width~sample,armdf,sum)[,2]
+    names(armlength) <- aggregate(arm.width~sample,armdf,sum)[,1]
+    part <- armdf$segmean * armdf$arm.width / armlength[armdf$sample]
+    
+    armdf <- data.frame(armdf,arm.width,part,armlength[armdf$sample])
+    
+    meanArmSegment <- aggregate(part~sample,armdf,sum)
+    
+    num <-  as.numeric(meanArmSegment[,2])
+    names(num) <- as.character(meanArmSegment[,1])
+    armcnvmat[arm,names(num)] <- num
+  }
+  return(armcnvmat)
+}
+
+
+
---
+++ R/match.breaks.r
@@ -0,0 +1,63 @@
+#' 
+#'
+#' Identify CNV breakpoints provided a segmentation file
+#' @param brk1 (data.frame) brakpoint coordinates 1 (sample, chrom, pos)
+#' @param brk2 (data.frame) brakpoint coordinates 1 (sample, chrom, pos)
+#' @param verbose (logical) 
+#' @keywords CNV, SV, genomic breakpoints
+#' @export
+#' @examples
+#' match.breaks()
+
+
+
+match.breaks <- function(brk1, brk2, 
+                          maxgap=50000,
+                          verbose=FALSE){
+  
+  require(GenomicRanges,quietly = TRUE,warn.conflicts = FALSE)
+  require(taRifx,quietly = TRUE,warn.conflicts = FALSE)
+ 
+  common_samples <- intersect(brk1$sample,brk2$sample)
+  stopifnot(length(common_samples) > 0, local = TRUE) 
+  
+  if(verbose){
+    message(paste("Finding common breaks for",length(common_samples),"common samples",sep=" "))
+    pb <- txtProgressBar(style=3)
+    cc <-0
+    tot <- length(common_samples)
+  }
+  
+  brk1_match <- brk2_match <- restab <- list()
+  for(id in common_samples){
+    
+    brk1_i <- brk1[which(brk1$sample == id),]
+    brk_ranges1 <- with(brk1_i, GRanges(chrom, IRanges(start=start, end=end)))
+    
+    brk2_i <- brk2[which(brk2$sample == id),]
+    brk_ranges2 <- with(brk2_i, GRanges(chrom, IRanges(start=start, end=end)))
+    
+    options(warn=-1)
+    seg_seg = GenomicAlignments::findOverlaps(brk_ranges1,brk_ranges2,maxgap=maxgap)
+    options(warn=0)
+    
+    brk_match1 <- sort(unique(queryHits(seg_seg)))
+    brk_match2 <- sort(unique(subjectHits(seg_seg)))
+    
+    restab[[id]] <- c(length(brk_match1), nrow(brk1_i), length(brk_match2), nrow(brk2_i))
+    names(restab[[id]]) <- c("matched.brk1", "total.brk1", "matched.brk2", "total.brk2")
+    
+    brk1_match[[id]] <- brk1_i[brk_match1,]
+    brk2_match[[id]] <- brk2_i[brk_match2,]
+    if(verbose) cc <- cc+1
+    if(verbose) setTxtProgressBar(pb, cc/tot)
+  }
+  if(verbose) close(pb)
+  
+  restab <- data.frame(do.call(rbind,restab))
+  return(list(
+    brk1_match = do.call(rbind,brk1_match),
+    brk2_match = do.call(rbind,brk2_match),
+    restab= restab))
+}
+
